f5b597c040bc015fea5882dd42ce79ca6ceca6c5
AnupJoseph <anup20joseph@mail.com>
2020-08-03 19:46:33 +0530

    Added keys and verification

--- tweepy_api.py
+++ tweepy_api.py
@@ -1,6 +1,7 @@
 import tweepy
-auth = tweepy.OAuthHandler("ZrMYVz4BAb4YdafAIceEVF7iU","Wxs0USX2wcqMd1cOorNLn58U4wS9mocLmHqRuETd97giJ4H4CX")
-auth.set_access_token("1284460786610368513-kQgCNaa42tjuRqB6pHnP2nLG7G0zPc", "nvlSUik8RK33xoVC3cxCJyz6i3r9Z8zGZ6JaVGzN588GI")
+auth = tweepy.OAuthHandler("1284460786610368513-kQgCNaa42tjuRqB6pHnP2nLG7G0zPc","nvlSUik8RK33xoVC3cxCJyz6i3r9Z8zGZ6JaVGzN588GI")
+auth.set_access_token("ZrMYVz4BAb4YdafAIceEVF7iU", "Wxs0USX2wcqMd1cOorNLn58U4wS9mocLmHqRuETd97giJ4H4CX")
+# auth.set_access_token("1284460786610368513-kQgCNaa42tjuRqB6pHnP2nLG7G0zPc", "nvlSUik8RK33xoVC3cxCJyz6i3r9Z8zGZ6JaVGzN588GI")
 
 try:
     api.verify_credentials()
