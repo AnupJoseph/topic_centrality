441256d4d47eac5d8fee39436d3032da05130189
AnupJoseph <anup20joseph@mail.com>
2020-08-04 18:43:57 +0530

    Incremental Change 2

--- tweepy_api.py
+++ tweepy_api.py
@@ -8,7 +8,7 @@ def get_tweets(screen_name):
 	Args:
 	    screen_name (string):User name under consideration  
 	"""
-	print(f"{"*"*5}Returning Tweets for {screen_name}")
+	print(f"Returning Tweets for {screen_name}")
 	tweets = tweepy.Cursor(api.user_timeline,screen_name=screen_name,include_rts=False,tweet_mode='extended')
 	print(tweets)
 
--- tweet_config.py
+++ tweet_config.py
@@ -4,7 +4,7 @@ CONSUMER_SECRET = "Wxs0USX2wcqMd1cOorNLn58U4wS9mocLmHqRuETd97giJ4H4CX"
 ACCESS_KEY      = "1284460786610368513-kQgCNaa42tjuRqB6pHnP2nLG7G0zPc"
 ACCESS_SECRET   = "nvlSUik8RK33xoVC3cxCJyz6i3r9Z8zGZ6JaVGzN588GI"
 
-print(f"{"*"*5}Authorizing Twitter; Initialize Tweepy{"*"*5}")
+print(f"Authorizing Twitter; Initialize Tweepy{"*"*5}")
 auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 auth.set_access_token(key=ACCESS_KEY, secret=ACCESS_SECRET)
 
