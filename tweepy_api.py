a768eb252b2f1d83164bb6b76cb33be8a6c0224b
AnupJoseph <anup20joseph@mail.com>
2020-08-04 18:38:12 +0530

    Added code for getting tweets

--- tweepy_api.py
+++ tweepy_api.py
@@ -1,12 +1,15 @@
 import tweepy
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("ZrMYVz4BAb4YdafAIceEVF7iU", "Wxs0USX2wcqMd1cOorNLn58U4wS9mocLmHqRuETd97giJ4H4CX")
-auth.set_access_token("1284460786610368513-kQgCNaa42tjuRqB6pHnP2nLG7G0zPc","nvlSUik8RK33xoVC3cxCJyz6i3r9Z8zGZ6JaVGzN588GI")
-api = tweepy.API(auth)
-# test authentication
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+from tweet_config import api
+
+def get_tweets(screen_name):
+	"""Function to get tweets of a specific user
+	
+	Args:
+	    screen_name (string):User name under consideration  
+	"""
+	print(f"{"*"*5}Returning Tweets for {screen_name}")
+	tweets = tweepy.Cursor(api.user_timeline,screen_name=screen_name,include_rts=False,tweet_mode='extended'method)
+	print(tweets)
+
+get_tweets("SenSanders")
---
+++ tweet_config.py
@@ -0,0 +1,11 @@
+import tweepy
+CONSUMER_KEY    = "ZrMYVz4BAb4YdafAIceEVF7iU"
+CONSUMER_SECRET = "Wxs0USX2wcqMd1cOorNLn58U4wS9mocLmHqRuETd97giJ4H4CX"
+ACCESS_KEY      = "1284460786610368513-kQgCNaa42tjuRqB6pHnP2nLG7G0zPc"
+ACCESS_SECRET   = "nvlSUik8RK33xoVC3cxCJyz6i3r9Z8zGZ6JaVGzN588GI"
+
+print(f"{"*"*5}Authorizing Twitter; Initialize Tweepy{"*"*5}")
+auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+auth.set_access_token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
