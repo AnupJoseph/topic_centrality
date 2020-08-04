ecdd3a055193dbec26445a4ab1bdb20f7767f733
AnupJoseph <anup20joseph@mail.com>
2020-08-04 19:10:40 +0530

    Incremental Change 7

--- preprocess.py
+++ preprocess.py
@@ -1,19 +1,46 @@
 import spacy
+import re
 from emoji import demojize
 nlp = spacy.load('en')
-# stopwords = nlp.Defaults.stop_words
-# print(stopwords)
+
 def emoji_to_text(line):
+  """Translate emoji to words as emoji hold infomration in tittwe
+  
+  Args:
+      line (String): Sentence for preprocessing
+  
+  Returns:
+      string: Sentence with emoji cleared 
+  """
   line = demojize(line)
   return line
+
+def remove_url(sentence):
+	"""Remove the token
+	
+	Args:
+	    sentence (TYPE): Description
+	
+	Returns:
+	    TYPE: Description
+	"""
+	return re.sub(r"http\S+", "", sentence)
+
 def preprocess(sentence):
+  """Preprocess framework
+  
+  Args:
+      sentence (String): Input String 
+  
+  Returns:
+      TYPE: Output string
+  """
   sentence = emoji_to_text(sentence)
   sentence = nlp(sentence)
-  sentence = [word for word in sentence if not word.is_punct]
+  sentence = [remove_url(word) for word in sentence]
+  sentence = [word for word in sentence if not word.is_punct or word == ""]
   sentence = [word for word in sentence if len(word)>3]
   sentence = [word for word in sentence if not word.is_stop]
   sentence = [word.lemma_ for word in sentence]
-  
+  # sentence = [remove_url(word) for word in sentence]
   return " ".join(sentence)
-# sent = preprocess("Wherever the maple leaf flies, it represents our ric'h history, our bright future, and the values that hold dear as Canadians.Happy flag day 🇨🇦")
-# print(sent)
