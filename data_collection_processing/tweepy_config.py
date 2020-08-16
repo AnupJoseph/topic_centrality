ec693442a16713ab13617814319e4fd7da68c298
AnupJoseph <anup20joseph@gmail.com>
2020-08-16 06:46:00 +0000

    Incremental Change

--- data_collection_processing/tweepy_config.py
+++ data_collection_processing/tweepy_config.py
@@ -1,4 +1,2 @@
 COLS = ['id', 'created_at', 'original_text','clean_text', 'retweet_count', 'hashtags','mentions', 'original_author']
-# politicians = ['SenSanders','realDonaldTrump', 'JoeBiden', 'andrewcuomo', 'TeamPelosi', 'NikkiHaley', 'MittRomney', 'Mike_Pence']
-
-politicians = ['SenatorCollins','PeteButtigieg']
+politicians = ['SenSanders','realDonaldTrump', 'JoeBiden', 'andrewcuomo', 'TeamPelosi', 'NikkiHaley', 'MittRomney', 'Mike_Pence','SenatorCollins','PeteButtigieg']
