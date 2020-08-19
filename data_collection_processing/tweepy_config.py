762c46324c7b18295e9feae67a79f9b858939b42
AnupJoseph <anup20joseph@gmail.com>
2020-08-19 06:35:13 +0000

    Locked login credentials

--- data_collection_processing/tweepy_config.py
+++ data_collection_processing/tweepy_config.py
@@ -2,11 +2,8 @@ import tweepy
 COLS = ['id', 'created_at', 'original_text','clean_text', 'retweet_count', 'hashtags','mentions', 'original_author']
 politicians = ['SenSanders','realDonaldTrump', 'JoeBiden', 'andrewcuomo', 'TeamPelosi', 'NikkiHaley', 'MittRomney', 'Mike_Pence','SenatorCollins','PeteButtigieg']
 RETWEET_COLS = ['original_tweet_id','retweet_id','type','created_at','source','favorite_count','retweet_count','original_author']
+from login_credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 
-CONSUMER_KEY    = "ZrMYVz4BAb4YdafAIceEVF7iU"
-CONSUMER_SECRET = "Wxs0USX2wcqMd1cOorNLn58U4wS9mocLmHqRuETd97giJ4H4CX"
-ACCESS_KEY      = "1284460786610368513-jL7KyWzoDAPR07BCIiEjKQoR2B29TY"
-ACCESS_SECRET   = "fdtTCHTXDMxzGmIx7xg2Xlz8k9e3OdYTuXGhKNm5zIueG"
 print("--- Authorize Twitter; Initialize Tweepy ---")
 auth 		= tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
