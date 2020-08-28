b1c1de983ec77d2d039eedffe5c00a3d20400ec9
AnupJoseph <43500348+AnupJoseph@users.noreply.github.com>
2020-08-28 19:00:54 +0530

    Added documentation to the file

--- data_collection_processing/preprocess.py
+++ data_collection_processing/preprocess.py
@@ -1,49 +1,56 @@
-import spacy
+# System imports
 import re
-from emoji import demojize
-from tweepy_config import COLS
+
+# External Imports
 import pandas as pd
+import spacy
+from emoji import demojize
+
+# Internal imports
+from tweepy_config import COLS
+
+# Intialize the spacy statistical model
 nlp = spacy.load('en_core_web_sm')
 
 def emoji_to_text(line):
-  """Translate emoji to words as emoji hold infomration in tittwe
-  
-  Args:
-      line (String): Sentence for preprocessing
-  
-  Returns:
-      string: Sentence with emoji cleared 
-  """
-  line = demojize(line)
-  return line
+    """Translate emoji to words as emoji hold important information in twitter
+
+    Args:
+        line ([string]): Input sentence with emoji as unicode characters
+
+    Returns:
+        [string]: Output sentence with the unicode emoji converted to text
+    """
+
+    line = demojize(line)
+    return line
 
 
 def process(sentence):
-  """Preprocess framework.Peforms the following operation\
 
-  * Convert emoji to root meaning
-  * Tokenization and creating a spacy doc
-  * Remove punctuation
-  * Removes words of less than 3 letters
-  * Removes Standard stop words (I'll add a custom set later)
-  * Removes urls
-  * Reduces each word to its root lemma 
-  
-  Args:
-      sentence (String): Input String 
-  
-  Returns:
-      TYPE: Output string after the above preprocessing 
-  """
-  sentence = emoji_to_text(sentence)
-  sentence = nlp(sentence)
-  
-  sentence = [word for word in sentence if not word.is_punct]
-  sentence = [word for word in sentence if len(word)>3]
-  sentence = [word for word in sentence if not word.is_stop]
-  sentence = [word for word in sentence if not word.like_url]
-  sentence = [word.lemma_ for word in sentence]
-  return " ".join(sentence)
+    """
+    Preprocess framework.Peforms the following operation\
+        * Convert emoji to root meaning
+        * Tokenization and creating a spacy doc
+        * Remove punctuation
+        * Removes words of less than 3 letters
+        * Removes Standard stop words (I'll add a custom set later)
+        * Removes urls
+        * Reduces each word to its root lemma 
+
+    Returns:
+        [string]: Output string after the above preprocessing
+    """
+    sentence = emoji_to_text(sentence)
+    sentence = nlp(sentence)
+    
+    sentence = [word for word in sentence if not word.is_punct]
+    sentence = [word for word in sentence if len(word)>3]
+    sentence = [word for word in sentence if not word.is_stop]
+    sentence = [word for word in sentence if not word.like_url]
+    sentence = [word.lemma_ for word in sentence]
+    
+    return " ".join(sentence)
 
 
 def cleaner(tweet):
@@ -51,7 +58,6 @@ def cleaner(tweet):
     Utility Function to serialize the workflow for cleaning up a tweets
     Args:
         tweet (Tweet object): [A tweet object containing all the information realted to a tweet]
-
     Returns:
         [list]: [List holding a cleaned object information as 
         'id', 'created_at', 'original_text','clean_text', 'retweet_count', 'hashtags','mentions', 'original_author']
@@ -67,7 +73,7 @@ def cleaner(tweet):
     cleaned_tweet.append(tweet.retweets)
 
 
-    # Use hashtags
+    # Use hashtags and add them to the list
     hashtags = "".join([hashtag_item for hashtag_item in tweet.hashtags])
     hashtags = hashtags if hashtags != '' else '<UNK>'
     cleaned_tweet.append(hashtags.strip())
