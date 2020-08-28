b79ab006465bdb05c17839839c494beb7ea43f1a
AnupJoseph <43500348+AnupJoseph@users.noreply.github.com>
2020-08-28 19:38:49 +0530

    Update write_to_file.py

--- data_collection_processing/write_to_file.py
+++ data_collection_processing/write_to_file.py
@@ -1,16 +1,20 @@
+# External Imports
 import pandas as pd
+
+# Internal imports
 from tweepy_config import RETWEET_COLS
 
 def merger(screen_name,data_frame):
+    """Merge the new dataframe with the prexisting file
+
+    Args:
+        screen_name ([string]): use name of the politicians
+        data_frame ([pandas.Dataframe]): new set of retweets to be appended to the existing file
+    """
+
 	final_csv = f'../data/{screen_name}/{screen_name}_retweets.csv'
-	# df = pd.read_csv(file_path)
 	with open(final_csv, 'a') as f:
 		data_frame.to_csv(f, header=f.tell()==0,columns=RETWEET_COLS)
+
 def write_to_file(file_path,new_data,screen_name):
-	"""
-		Writes a dataframe to file
-	"""
-	data_frame = new_data
-	# csvFile = open(file_path, 'w' ,encoding='utf-8')
-	# data_frame.to_csv(csvFile, mode='w', index=False, encoding="utf-8")
-	merger(screen_name,data_frame)
+	merger(screen_name,new_data)
