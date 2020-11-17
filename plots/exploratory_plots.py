a5360170f1ca2418cfd92f78694c30e8eab4e694
AnupJoseph <anup20joseph@gmail.com>
2020-11-17 10:29:04 +0530

    added combined dataframe

--- plots/exploratory_plots.py
+++ plots/exploratory_plots.py
@@ -8,21 +8,22 @@ import plotly
 politicians = ['SenSanders', 'realDonaldTrump', 'JoeBiden', 'andrewcuomo', 'TeamPelosi',
                'NikkiHaley', 'MittRomney', 'Mike_Pence', 'SenatorCollins', 'PeteButtigieg']
 
+def save_to_excel(dataframe_list,outfolder= 'excels'):
+    import os
+    if not os.path.exists(outfolder):
+        os.makedirs(outfolder)
+    writer = pd.ExcelWriter('Combined_politicians_list.xlsx', engine='xlsxwriter')
+    for index,tables in enumerate(dataframe_list):
+        tables.to_excel(writer,sheet_name=politicians[index])
+    writer.save()
 
 frames = []
-for politician in politcians:
+for politician in politicians:
     file_path = f"../data/{politician}/{politician}_data.csv"
-    timeline_df = pd.read_csv(file_path)
-    frames.append(timeline_df)
-total_df = pd.concat(frames,sort=False)  
+    dataframe = pd.read_csv(file_path)
+    dataframe['Date'] = pd.to_datetime(dataframe['created_at']).dt.date
+    dataframe = dataframe[['created_at','Date']]
+    dataframe = dataframe.groupby('Date').count()
+    frames.append(dataframe)
 
-total_df["Date"] = pd.to_datetime(total_df['created_at']).dt.date
-tweets_over_time = total_df[["Date","created_at","original_author"]]
-tweets_over_time["Cumulative"] = total_df["created_at"]
-tweets_over_time = tweets_over_time.drop("created_at",axis=1)
-tweets_over_time = tweets_over_time.groupby(tweets_over_time["Date"]).count()
-
-tweets_over_time.to_csv('final.csv')
-cumulative_tweets = tweets_over_time["Cumulative"].cumsum()
-
-cumulative_tweets.to_csv('cumul_final.csv')
+save_to_excel(frames,'trends')
