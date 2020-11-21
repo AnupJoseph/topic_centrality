ecd1a7a1f164a5942f719c57ab52b7d92e01d058
AnupJoseph <anup20joseph@gmail.com>
2020-11-21 20:09:34 +0530

    Added setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
 python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_sm
+apt-get update
+apt-get install openjdk-8-jdk-headless -qq > /dev/null --fix-missing
+wget https://mirrors.estointernet.in/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
+tar xvf spark-3.0.1-bin-hadoop2.7.tgz
+pip install -q findspark
+pip install pyLDAvis textacy -qq > /dev/null
