edb63faee0e7c8ea7dc53d59466ece3f07baca22
AnupJoseph <anup20joseph@gmail.com>
2020-11-22 13:34:20 +0530

    Added dash Table

--- app/apps/dataset.py
+++ app/apps/dataset.py
@@ -2,9 +2,21 @@ import dash
 import dash_table
 import pandas as pd
 
-df = pd.read_csv('https://github.com/AnupJoseph/topic_centrality/blob/master/data/sparkling_clean_data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/AnupJoseph/topic_centrality/master/data/sparkling_clean_data.csv')
 layout = dash_table.DataTable(
+    style_cell={
+        'overflow': 'hidden',
+        'textOverflow': 'ellipsis',
+        'maxWidth': 0,
+    },
     id='table',
     columns=[{"name": i, "id": i} for i in df.columns],
     data=df.to_dict("rows"),
+    tooltip_data=[
+        {
+            column: {'value': str(value), 'type': 'markdown'}
+            for column, value in row.items()
+        } for row in df.to_dict('rows')
+    ],
+    tooltip_duration=None
 )
