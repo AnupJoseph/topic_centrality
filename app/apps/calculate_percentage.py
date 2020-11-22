b8bebe4c3bfe3cf13e742dde3e3065fb24ce4af4
AnupJoseph <anup20joseph@gmail.com>
2020-11-22 19:38:58 +0530

    Added fix

--- app/apps/calculate_percentage.py
+++ app/apps/calculate_percentage.py
@@ -13,11 +13,13 @@ def calculate_percentage():
     data = pd.DataFrame(columns=COLS)
     for politician in politicians:
         df = pd.read_csv(f"../data/{politician}/{politician}_data_temp.csv")
-        df.drop(labels=['Unnamed: 0','Unnamed: 0.1'],inplace=True)
+        # df.drop(labels=['Unnamed: 0','Unnamed: 0.1'],inplace=True)
+        df.drop('Unnamed: 0',inplace=True,axis=1)
+        df.drop('Unnamed: 0.1',inplace=True,axis=1)
         data = pd.concat([data,df])
     
     percentage_data = Counter(data['lda_cluster'])
 
-    total = sum(percentage_data.values)
-    return[(item/total)*100 for item in percentage_data.values]
+    total = sum(percentage_data.values())
+    return[(item/total)*100 for item in percentage_data.values()]
         
