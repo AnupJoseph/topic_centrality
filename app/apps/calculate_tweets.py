b78c089678c1f9f904e3484777b28566f8ec8b57
AnupJoseph <anup20joseph@gmail.com>
2020-11-22 20:26:28 +0530

    Added bar chart

---
+++ app/apps/calculate_tweets.py
@@ -0,0 +1,26 @@
+import os.path
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import pandas as pd
+from collections import Counter
+import operator
+
+def calculate_tweets(n):
+    politicians = ['SenSanders', 'realDonaldTrump', 'JoeBiden', 
+    # 'andrewcuomo', 'TeamPelosi','NikkiHaley', 'MittRomney', 
+                   'Mike_Pence', 
+                #    'SenatorCollins', 
+                   'PeteButtigieg']
+    dataset = pd.DataFrame(index=range(-1,7))
+
+    # for i in range(n):
+    for politician in politicians:
+        df = pd.read_csv(f"../data/{politician}/{politician}_data_temp.csv")
+        data = Counter(df['lda_counts'])
+        data = [(key,value) for key,value in data.items()]
+        data = sorted(data,key=operator.itemgetter(0))
+        dataset[politician] = pd.DataFrame([i[1] for i in data],index=[i[0] for i in data])
+    
+    return dataset
+        
--- app/apps/lda_topic_dist.py
+++ app/apps/lda_topic_dist.py
@@ -15,8 +15,30 @@ def topic_fig(series,n):
     fig.update_traces(textposition='outside', textinfo='percent+label')
     return fig
 
+from .calculate_tweets import calculate_tweets
+def tweet_topic_fig(leader_dataframe,n):
+    colors = {
+        0:'#003f5c',
+
+    }
+    fig = go.Figure()
+    labels = [f'Topic {i}' for i in range(n)]
+    for index,leader in enumerate(leader_dataframe.columns):
+        fig.add_trace(go.Bar(x=labels,
+                y=leader_dataframe[leader],
+                name=leader,
+                marker_color=colors[index]
+                ))
+    fig.update_layout(
+    title='US Export of Plastic Scrap',
+    xaxis_tickfont_size=14,
+    barmode='group',
+    bargap=0.15, # gap between bars of adjacent location coordinates.
+    bargroupgap=0.1)
+    return fig
 
 percentages = calculate_percentage()
+leader_dataframe = calculate_tweets()
 layout = html.Div([
     dbc.Container([
         dbc.Row([
@@ -26,6 +48,14 @@ layout = html.Div([
     ]),
     dbc.Row([
         dcc.Graph(figure=topic_fig(percentages,7))
+    ]),
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H1(children='Topic distribution by leader in tweets at a glance'),
+                className="mb-6")
     ])
-
+    ]),
+    dbc.Row([
+        dcc.Graph(figure=tweet_topic_fig(percentages,7))
+    ]),
 ])
